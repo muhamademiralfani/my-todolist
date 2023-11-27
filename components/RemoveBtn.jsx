fbd2f00ffd2cf0b02f0478c30934fa1838907942
muhamad_emir <muhamademiralfani@gmail.com>
2023-11-27 23:02:38 +0700

    Backend Aplikasiya (finish)

--- components/RemoveBtn.jsx
+++ components/RemoveBtn.jsx
@@ -1,8 +1,25 @@
-import { HiOutlineTrash } from "react-icons/hi";
+"use client";
 
-export default function RemoveBtn() {
+import { HiOutlineTrash } from "react-icons/hi";
+import { useRouter } from "next/navigation";
+
+export default function RemoveBtn({ id }) {
+  const router = useRouter();
+  const removeTopic = async () => {
+    const confirmed = confirm("Are you sure ?");
+
+    if (confirmed) {
+      const res = await fetch(`http://localhost:3000/api/topics?id=${id}`, {
+        method: "DELETE",
+      });
+      if (res.ok) {
+        router.refresh();
+      }
+    }
+  };
+
   return (
-    <button className="text-red-500">
+    <button onClick={removeTopic} className="text-red-500">
       <HiOutlineTrash size={24} />
     </button>
   );
--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -1,23 +1,42 @@
+/* eslint-disable react/jsx-key */
 import Link from "next/link";
 import RemoveBtn from "./RemoveBtn";
 import { HiPencilAlt } from "react-icons/hi";
 
-export default function TopicsList() {
+const getTopics = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/topics", {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch topics");
+    }
+    return res.json();
+  } catch (error) {
+    console.log("Error loading topics: ", error);
+  }
+};
+
+export default async function TopicsList() {
+  const { topics } = await getTopics();
+
   return (
     <>
-      <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start ">
-        <div>
-          <h2 className="font-bold text-2xl">Topic Title</h2>
-          <div>Topic Description</div>
+      {topics.map((t) => (
+        <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start ">
+          <div>
+            <h2 className="font-bold text-2xl">{t.title}</h2>
+            <div>{t.description}</div>
+          </div>
+          <div className="flex gap-2">
+            <RemoveBtn id={t._id} />
+            <Link href={`/editTopic/${t._id}`}>
+              <HiPencilAlt size={24} />
+            </Link>
+          </div>
         </div>
-
-        <div className="flex gap-2">
-          <RemoveBtn />
-          <Link href={"/editTopic/123"}>
-            <HiPencilAlt size={24} />
-          </Link>
-        </div>
-      </div>
+      ))}
     </>
   );
 }
